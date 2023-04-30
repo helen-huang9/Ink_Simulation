a518639bb31b59a666b696c44ae2fce60622f27f
Helen Huang <helen_huang@brown.edu>
2023-04-30 17:44:25 -0400

    Initial Commit

---
+++ Ink_Simulation/Ink_SimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ink_SimulationApp.swift
+//  Ink_Simulation
+//
+//  Created by Helen Huang on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Ink_SimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
